4178c4d937727e4c6b6c84aac93a841fe47b6ccc
veblen <407497818@qq.com>
2022-11-07 18:19:38 +0800

    feat: :sparkles: 首页界面列表和导航样式

---
+++ src/components/navBar.tsx
@@ -0,0 +1,100 @@
+import { useState } from "react";
+import { MenuOutlined } from "@ant-design/icons";
+import { Button, Drawer } from "antd";
+import Link from "next/link";
+import Image from 'next/image'
+type Props = {
+  menuShow: boolean;
+  setMenuShow: any;
+};
+const menuName: {
+  name: string;
+  url: string;
+}[] = [
+  {
+    name: "首页",
+    url: "/",
+  },
+  { name: "标签", url: "/tag" },
+  {
+    name: "分类",
+    url: "/category",
+  },
+  {
+    name: "时间线",
+    url: "/timeline",
+  },
+  {
+    name: "关于",
+    url: "/about",
+  },
+];
+const Menu = (props: Props) => {
+  return (
+    <>
+      <nav className="hidden sm:flex items-center">
+        <ul className={"flex items-center"}>
+          {menuName.map((item) => (
+            <li
+              key={item.name}
+              className={
+                "ml-10 h-10 hover:border-b-2 hover:border-blue-600  leading-10"
+              }
+            >
+              <Link href={item.url} className="block">
+                <a> {item.name}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      <Drawer
+        open={props.menuShow}
+        closable={false}
+        placement={"left"}
+        width={250}
+        onClose={() => props.setMenuShow(false)}
+      >
+        <div>
+          <Image
+            src="https://img2.baidu.com/it/u=1395017289,2375190034&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1666803600&t=9e75e8156906bf586ba690a2ab8269c6"
+            alt="头像"
+            width={50}
+            height={50}
+          />
+        </div>
+        <nav>
+          <ul className='divide-y-4 divide-yellow-600 divide-dashed'>
+            {menuName.map((item) => (
+              <li key={item.name} className="py-2 ">
+                <Link href={item.url} className="block">
+                  <a> 菜单{item.name}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </Drawer>
+    </>
+  );
+};
+
+export const NavBar = () => {
+  const [menuShow, setMenuShow] = useState(false);
+
+  return (
+    <header className="flex h-14 bg-white items-center px-4 sm:px-10">
+      {/*<div className={'flex items-center'}>*/}
+      <MenuOutlined
+        className="text-lg sm:hidden"
+        onClick={() => setMenuShow(true)}
+      />
+      <Menu menuShow={menuShow} setMenuShow={setMenuShow}></Menu>
+
+      {/*</div>*/}
+      {/*<Menu></Menu>*/}
+      <div></div>
+    </header>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,60 +1,51 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-// import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
-import { LikeOutlined, MessageOutlined, StarOutlined } from '@ant-design/icons';
-import type { MenuProps } from 'antd';
-import { Menu,Layout,Avatar, List, Space  } from 'antd';
-import React, { useState } from 'react';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import dayjs from 'dayjs'
+import { Divider } from 'antd';
 
-const { Header, Footer, Sider, Content } = Layout;
+import { NavBar } from "../components/navBar";
 
-const items: MenuProps['items'] = [
-    { label: '菜单项一', key: 'item-1' }, // 菜单项务必填写 key
-    { label: '菜单项二', key: 'item-2' },
-    {
-        label: '子菜单',
-        key: 'submenu',
-        children: [{ label: '子菜单项', key: 'submenu-item-1' }],
+export async function getStaticProps() {
+  const allPostsData = await fetch("http://127.0.0.1:3001/api_v1/post").then(
+    (r) => r.json()
+  );
+
+  return {
+    props: {
+      allPostsData: allPostsData,
     },
-];
-
-
-const Menus: React.FC = () => {
-    const [current, setCurrent] = useState('mail');
-
-    const onClick: MenuProps['onClick'] = e => {
-        console.log('click ', e);
-        setCurrent(e.key);
-    };
-    const style={
-        display:'flex',
-        justifyContent:'center'
-    }
-
-    return <Menu onClick={onClick} style={style} selectedKeys={[current]} mode="horizontal" items={items} />;
-
-};
-
-
-
-
-const Home: NextPage = () => {
-  return (
-     <div className='bg-amber-200 w-full h-screen'>
-       <Head>
-           <title>首页</title>
-       </Head>
-
-
-
-             <header><Menus/></header>
-            <main className='w-3/4 m-auto'>
-            </main>
-
-     </div>
-)
-
+  };
 }
 
-export default Home
+const Home: NextPage = ({ allPostsData }: any) => {
+    console.log(allPostsData.data.data)
+  return (
+    <div className="bg-amber-200 w-full h-screen">
+      <Head>
+        <title>首页</title>
+      </Head>
+      <NavBar />
+
+      <main className="w-3/4 m-auto  mt-3">
+        <ul>
+          {allPostsData.data.data.map((item: any) => {
+            return (
+              <li key={item.id} className={"bg-white m-2 p-4 rounded-lg"}>
+                <h2 className={"text-lg"}>{item.title}</h2>
+                <p className={"text-sm my-2 h-14"}>{item.summary}</p>
+                <div className={"text-sm my-2 text-xs"}>
+                   <span> {dayjs(item.creatTime).format('YYYY-MM-DD HH:mm')}</span>
+                    <Divider type="vertical" />
+                    <span>阅读:{item.count}</span>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
