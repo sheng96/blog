018a6678b16e283e5738cb2cbce7c6f1c0ceba45
veblen <407497818@qq.com>
2022-11-07 18:18:05 +0800

    feat: :sparkles: 增加代理解决跨域问题

--- next.config.js
+++ next.config.js
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['img2.baidu.com'],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api_v1/:slug*',
+        destination: 'http://119.29.15.252:8888/:slug*'
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
