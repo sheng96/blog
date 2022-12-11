27271dc907f06ea4fe0cf6b64e7ec867ad27048f
sheng <407497818@qq.com>
2022-12-12 00:07:55 +0800

    build: :package: 更新依赖

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 
 import type { AppProps } from 'next/app'
-import 'antd/dist/antd.css'
+// import 'antd/dist/antd.css'
 import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
