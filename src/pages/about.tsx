eef3d948b610030009487c2b699bbbc4c140db8a
sheng <407497818@qq.com>
2023-02-11 17:00:27 +0800

    feat: 增加更新日志功能，关于页面描述修改

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "@/components/Layout";
-import style from '../styles/about.module.scss'
+import style from "../styles/about.module.scss";
+import {Tag} from "antd";
 const About = () => {
   return <Layout content={Content()}></Layout>;
 };
@@ -8,63 +9,66 @@ const Content = () => {
     <main
       className={`w-full md:w-11/12   lg:w-9/12 xl:w-8/12 m-auto bg-white mt-6 px-6 py-4 `}
     >
-      <div className={style.about_html}>
-        <h3 id="🖥️关于本站" className={`text-3xl font-bold`}>🖥️关于本站</h3>
-        <p >
+      <div className={style.person}>
+        <h3  className={`text-3xl font-bold`}>
+          关于我
+        </h3>
+        <p className={`mt-6`}>
+          <Tag color="#2db7f5">当前位置</Tag>
+          <span>广州</span>
+        </p>
+        <p className={`mt-3`}>
+          <Tag color="#87d068">关于工作</Tag>
+          <span>前端开发工程师</span>
+        </p>
+
+      </div>
+
+
+
+
+      <div className={style.about_html+` mt-10`}>
+        <h3 id="关于本站" className={`text-3xl font-bold`}>
+          关于本站
+        </h3>
+        <p>
           本站是自己学习了<strong>React</strong>后，用<strong>React</strong>
           写的个人博客系统，包括<strong>博客展示页面</strong>和
           <strong>后台管理页面</strong>，现在看到的就是
           <strong>博客展示页面</strong>~
         </p>
-        <p >
+        <p>
           主要整理、分享一些自己的学习笔记和心得，若有错误，欢迎大家指出、交流！
         </p>
-        <p >
-          ✅目前<strong>博客展示页面</strong>已成功升级<code>2.0</code>
-          版本，样式基本保持原样，主要是逻辑代码进行了重构，优化了整体的性能😎。
+        <p>
+          目前<strong>博客展示页面</strong>已成功部署
         </p>
-        <p >
-          ❓<strong>后台管理页面</strong>
+        <p>
+          <strong>后台管理页面</strong>
           使用的是vue3，<strong>目前</strong>
-          还没有完善。
+          仅仅有发布和删除功能。
         </p>
-        <p >🔖博客主要使用到的库如下：</p>
-        <p >
+        <p>博客主要使用到的库如下：</p>
+        <p>
           <strong>前端</strong>（博客展示页面）：
         </p>
         <ul>
-          <li>
-            主要技术栈为<code>react</code>+<code>hooks</code>+<code>TS</code>
-          </li>
+
           <li>
             使用
-            <a href="https://github.com/lzxjack/my-react" target="_blank" rel="noreferrer">
+            <code>
+            <a
+              href="https://github.com/lzxjack/my-react"
+              target="_blank"
+              rel="noreferrer"
+            >
               nextjs
-            </a>
+            </a></code>
             开发
           </li>
           <li>
             <code>AntD</code>组件库（自定义样式/按需导入）
           </li>
-          {/*<li>*/}
-          {/*  <code>ahooks</code>库提供常用的<code>hooks</code>*/}
-          {/*</li>*/}
-          <li>
-            <code>axios</code>网络请求库
-          </li>
-          {/*<li>*/}
-          {/*  <code>echarts</code>图标库绘制饼图*/}
-          {/*</li>*/}
-          <li>
-            时间格式化工具<code>dayjs</code>
-          </li>
-          <li>
-            <code>markdown</code>格式渲染工具<code>marked</code>
-          </li>
-          <li>
-            代码高亮渲染工具<code>highlight.js</code>
-          </li>
-          <li>其他常用工具库等</li>
         </ul>
         <p>
           <strong>后端</strong>：
--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -1,13 +1,45 @@
 import { Layout } from "@/components/Layout";
+import { Timeline } from "antd";
+import { SmileOutlined } from "@ant-design/icons";
 
-const def =()=> {
-    return <Layout content={FC()}></Layout>;
+const def = () => {
+  return <Layout content={FC()}></Layout>;
 };
 
-const FC=()=>{
-    return <main className={`w-fit mt-3`}>
-        这是一个时间线
+const FC = () => {
+  return (
+    <main className={`w-3/5 p-6  bg-white m-auto mt-3 `}>
+        <h3 className={`mb-10 text-4xl font-bold text-center text-cyan-400`}>日志</h3>
+      <Timeline
+        items={[
+          {
+            children: (
+              <>
+                <div className={`text-base text-gray-500 font-bold`}>2023-02-11</div>
+                <p
+                  className={`bg-slate-500 rounded w-96 shadow-2xl text-white p-3 mt-2`}
+                >
+                 添加日志页面和关于页面
+                </p>
+              </>
+            ),
+          },
+          {
+            children: (
+              <>
+                <div className={`text-base text-gray-500 font-bold`}>2022-11-06</div>
+                <p
+                  className={`bg-slate-500 rounded w-96 shadow-2xl text-white p-3 mt-2`}
+                >
+                  博客上线
+                </p>
+              </>
+            ),
+          },
+        ]}
+      />
     </main>
-}
+  );
+};
 
-export default def
+export default def;
