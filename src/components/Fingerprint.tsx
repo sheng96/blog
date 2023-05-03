b042a99e0b4c7fc4f393bbef7ef7873f5fb6b9c9
sheng <407497818@qq.com>
2023-05-03 22:43:41 +0800

    feat: 新增浏览器指纹

--- src/components/Fingerprint.tsx
+++ src/components/Fingerprint.tsx
@@ -1,20 +1,22 @@
 import FingerprintJS from "@fingerprintjs/fingerprintjs";
-export default async function (){
+import React, { useEffect } from 'react';
+function Fingerprint(){
+    useEffect(() => {
+        FingerprintJS.load().then((fp) => {
+            return fp.get()
+        }).then((result) => {
+            console.log(result.visitorId)
+            // 读取本地储存中的浏览器指纹
+            const _fp = localStorage.getItem("fp_token");
+            if (!_fp) {
+                localStorage.setItem("fp_token",result.visitorId);
+            }
+        })
+    },[])
+    // const { canvas,languages,colorDepth,colorGamut,osCpu,platform,timezone,vendor,videoCard,audio} = result.components
+    // console.log(FingerprintJS.hashComponents({canvas,languages,colorDepth,colorGamut,osCpu,platform,timezone,vendor,videoCard,audio}))
 
-    const fpPromise = FingerprintJS.load();
-    // 读取本地储存中的浏览器指纹
-    const _fp = localStorage.getItem("fp_token");
-    if (!_fp) {
-        const fp = await fpPromise;
-        const result = await fp.get();
-        localStorage.setItem("fp_token", result.visitorId);
-    }
-    const fp = await fpPromise;
-    const result = await fp.get();
-    // console.log(result)
-    console.log(result.visitorId)
-    const { canvas,languages,colorDepth,colorGamut,osCpu,platform,timezone,vendor,videoCard,audio} = result.components
-    console.log(FingerprintJS.hashComponents({canvas,languages,colorDepth,colorGamut,osCpu,platform,timezone,vendor,videoCard,audio}))
-
- return (<></>)
+ return null
 }
+
+export default Fingerprint
