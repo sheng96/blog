f31133da237bd6899f0c92883049d4f746fa5c18
sheng <407497818@qq.com>
2023-09-06 23:05:53 +0800

    添加阅读数量记录

--- src/api/post.ts
+++ src/api/post.ts
@@ -4,25 +4,30 @@
 import http from '@/utils/http/axios'
 // import { statusEnum } from '@/api/model/postModel'
 import { BasicPageParams } from '@/api/model/baseModel'
-import {ArchiveModel, PostDetailModel, PostListModel} from "@/api/model/postModel";
+import {
+  ArchiveModel,
+  PostDetailModel,
+  PostListModel
+} from '@/api/model/postModel'
 
-export const postAllApi = async (params: BasicPageParams = {}):Promise<PostListModel> =>
-    await http.get('/post', {
-      params
-    })
-export const PostDetail = async (id:number):Promise<PostDetailModel> =>
-    await http.get(`/post/${id}`, )
-export const archiveApi = async ():Promise<ArchiveModel> =>
-    await http.get(`/post/archive`, )
+export const postAllApi = async (
+  params: BasicPageParams = {}
+): Promise<PostListModel> =>
+  await http.get('/post', {
+    params
+  })
+export const PostDetail = async (id: number): Promise<PostDetailModel> =>
+  await http.get(`/post/${id}`)
+export const archiveApi = async (): Promise<ArchiveModel> =>
+  await http.get(`/post/archive`)
 
 interface SearchParams extends BasicPageParams {
-    keyword:string
+  keyword: string
 }
 
-export const searchApi = async (params:SearchParams):Promise<PostListModel> =>
-    await http.get(`/post/search`,{
-        params
-    } )
+export const searchApi = async (params: SearchParams): Promise<PostListModel> =>
+  await http.get(`/post/search`, {
+    params
+  })
 
-
-
+export const addCountApi = async (id: number) => await http.patch(`/post/${id}`)
